0b9cbcd70b87aaa5ff282b98879d4a285e555718
bartekkolodziej <bartekko@student.agh.edu.pl>
2017-03-05 22:49:23 +0100

    Add files via upload

---
+++ lab2/array2d/Array2D.h
@@ -0,0 +1,17 @@
+//
+// Created by kcik1_000 on 2017-03-05.
+//
+
+#ifndef JIMP_EXERCISES_ARRAY2D_H
+#define JIMP_EXERCISES_ARRAY2D_H
+
+#include<iostream>
+
+using namespace std;
+
+int **Array2D(int n_rows, int n_columns);
+void DeleteArray2D(int **array, int n_rows);
+void FillArray2D(int n_rows, int n_columns,int **array);
+void Print(int **array, int n_rows, int n_columns);
+
+#endif //JIMP_EXERCISES_ARRAY2D_H
---
+++ lab2/array2d/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by kcik1_000 on 2017-03-05.
+//
+#include "Array2D.h"
+
+using namespace std;
+
+int main() {
+    int rows, columns;
+    cout << "Ile wierszy? \n";
+    cin >> rows;
+    cout << "Ile kolumn? \n";
+    cin >> columns;
+    int **tab = Array2D(rows,columns);
+
+
+    FillArray2D(rows, columns,tab);
+    Print(tab,rows,columns);
+    DeleteArray2D(tab, rows);
+
+return 0;
+}
