46c6242e6cd7a25a95c73845932d2c65d1d1c57b
Bartłomiej Kołodziej <bartekko@student.agh.edu.pl>
2017-03-21 15:50:05 +0100

    dynamic array

---
+++ lab4/dynamicarray/DynamicArray.cpp
@@ -0,0 +1,13 @@
+//
+// Created by bartek on 21.03.17.
+//
+
+#include "DynamicArray.h"
+
+DTab::DTab(){
+    double *tab = new double [10];
+}
+
+DTab::DTab(int length){
+    double *tab = new double [length];
+}
