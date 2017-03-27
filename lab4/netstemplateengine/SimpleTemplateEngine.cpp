1b73adf06d0345aae145672bba5aa9b97ee73813
Bartłomiej Kołodziej <bartekko@student.agh.edu.pl>
2017-03-27 20:01:06 +0200

    nets template engine everything works

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -12,32 +12,32 @@ namespace nets{
     std::string View::Render(const std::unordered_map <std::string, std::string> &model) const{
         std::string new_text = "";
         std::string tmp = "";
-        for(auto x: model) {
-            for(int i=0; i < raw_text.size(); i++){
-                if(raw_text[i] == '{' && raw_text[i+1] == '{' && raw_text[i+2] != '{'){
-                    i += 2;
-                    while(raw_text[i] != '}'){
-                        tmp += raw_text[i];
-                        i++;
-                    }
-                    if(raw_text[i+1] == '}'){
+        for(int i=0; i < raw_text.size(); i++){
+            if(raw_text[i] == '{' && raw_text[i+1] == '{' && raw_text[i+2] != '{'){
+                i += 2;
+                while(raw_text[i] != '}'){
+                    tmp += raw_text[i];
+                    i++;
+                }
+                if(raw_text[i+1] == '}'){
+                    for(auto x: model){
                         if(tmp == x.first) {
                             new_text += x.second;
                             i++;
                             tmp = "";
                         }
-                        else {
-                            tmp = "";
-                            i++;
-                        }
                     }
-                    else {
-                        new_text += "{{" + tmp + "}";
+                    if(tmp != "") {
                         tmp = "";
+                        i++;
                     }
                 }
-                else new_text += raw_text[i];
+                else {
+                    new_text += "{{" + tmp + "}";
+                    tmp = "";
+                }
             }
+            else new_text += raw_text[i];
         }
         return new_text;
     }
--- lab4/netstemplateengine/SimpleTemplateEngine.h
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -16,7 +16,7 @@ namespace nets {
     ~View();
     std::string Render(const std::unordered_map<std::string, std::string> &model) const;
         std::string raw_text;
-};
+    };
 }
 
 #endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
