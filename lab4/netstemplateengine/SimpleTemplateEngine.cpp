e1cfd2179129d72b387d011e3ad955ad0ce8b65c
Bartłomiej Kołodziej <bartekko@student.agh.edu.pl>
2017-03-27 16:16:03 +0200

    nets template engine 8 tests done, 3 failed

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -10,8 +10,37 @@ namespace nets{
     }
     View::~View(){}
     std::string View::Render(const std::unordered_map <std::string, std::string> &model) const{
-        std::regex pattern {R"(({{(?<subexpression>\w+)}}))"};
-        std::string rendered_text=std::regex_replace(this->raw_text, pattern, model["&{subexpression}"]);
-        return rendered_text;
+        std::string new_text = "";
+        std::string tmp = "";
+        for(auto x: model) {
+            for(int i=0; i < raw_text.size(); i++){
+                if(raw_text[i] == '{' && raw_text[i+1] == '{' && raw_text[i+2] != '{'){
+                    i += 2;
+                    while(raw_text[i] != '}'){
+                        tmp += raw_text[i];
+                        i++;
+                    }
+                    if(raw_text[i+1] == '}'){
+                        if(tmp == x.first) {
+                            new_text += x.second;
+                            i++;
+                            tmp = "";
+                        }
+                        else {
+                            tmp = "";
+                            i++;
+                        }
+                    }
+                    else {
+                        new_text += "{{" + tmp + "}";
+                        tmp = "";
+                    }
+                }
+                else new_text += raw_text[i];
+            }
+        }
+        return new_text;
     }
+
 }
+
