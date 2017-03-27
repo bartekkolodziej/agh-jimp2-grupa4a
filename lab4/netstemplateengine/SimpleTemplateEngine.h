b66ba9627ef85133233c1fc72c84b72a8c237e8c
bartosz <6bartek9@gmail.com>
2017-03-27 13:15:19 +0200

    Not sure whats wrong.

---
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -0,0 +1,23 @@
+//
+// Created by bartosz on 26.03.17.
+//
+
+#ifndef JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
+#define JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
+
+#include <string>
+#include <regex>
+#include <unordered_map>
+
+namespace nets {
+    class View {
+    public:
+    View(std::string text);
+    ~View();
+    std::string Render(const std::unordered_map<std::string, std::string> &model) const;
+    private:
+            std::string raw_text;
+};
+}
+
+#endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
