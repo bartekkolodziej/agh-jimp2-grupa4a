ef1a7ade7c8bdd13c25ad391987dbc6162e95003
bartosz <6bartek9@gmail.com>
2017-05-01 19:48:07 +0200

    Not working yet.

--- lab8/moviesubs/MovieSubtitles.cpp
+++ lab8/moviesubs/MovieSubtitles.cpp
@@ -6,22 +6,53 @@
 
 namespace moviesubs{
 
+    bool IsNotNumber(const char &x ){
+        std::string dictionary = "1234567890";
+        for(auto i: dictionary){
+            if (i == x) return false;
+        }
+        return true;
+
+    }
+
     void MicroDvdSubtitles::ShiftAllSubtitlesBy(int delay, int framerate, std::stringstream *in, std::stringstream *out){
         std::string text, frame;
         text = (*in).str();
-
+        int line=0;
+        if(framerate<=0) throw WrongFramerateException(framerate);
         for(int i = 0; i < text.length(); i++){
-            if(text[i] == '{'){
-                frame = "";
-                i++;
-                while(text[i] != '}') {
-                    frame += text[i];
+            if(text[i] ==  '\\' and text[i + 1] == 'n') line++;
+            if(text[i] == '}') throw InvalidSubtitleLineFormat();
+                if(text[i] == '{'){
+                    frame = "";
                     i++;
-                }
-                *out << '{' + std::to_string(atoi(frame.c_str()) + (delay*framerate)/1000) + '}';
+                    if(text[i] == '}') throw InvalidSubtitleLineFormat();
+                    while(text[i] != '}') {
+                        if(IsNotNumber(text[i]))throw InvalidSubtitleLineFormat();
+                        frame += text[i];
+                        i++;
+                    }
+                    frame = "";
+                    i++;
+                    if(text[i] != '{') throw InvalidSubtitleLineFormat();
+                    int start = atoi(frame.c_str());
+                    if (start<0) throw NegativeFrameAfterShift();
+                    while(text[i] != '}') {
+                        if(IsNotNumber(text[i]))throw InvalidSubtitleLineFormat();
+                        frame += text[i];
+                        i++;
+                    }
+                    int end = atoi(frame.c_str());
+                    if (end<0) throw NegativeFrameAfterShift();
+                    if (start<end) throw SubtitleEndBeforeStart(line);
+                    *out << '{' + std::to_string(start + (delay*framerate)/1000) + '}'+'{' + std::to_string(end + (delay*framerate)/1000) + '}';
             }
             else *out << text[i];
         }
+    }
+
+    int SubtitleEndBeforeStart::LineAt() const{
+        return line_;
     }
 
 
--- lab8/moviesubs/MovieSubtitles.h
+++ lab8/moviesubs/MovieSubtitles.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <sstream>
+#include <stdexcept>
 
 
 namespace moviesubs {
@@ -41,6 +42,42 @@ namespace moviesubs {
 
     };
 
+
+    class MovieSubtitlesError : public std::runtime_error{
+    public:
+        MovieSubtitlesError();
+        MovieSubtitlesError(std::string error_message);
+
+        virtual ~MovieSubtitlesError();
+    };
+
+    class WrongFramerateException : public std::invalid_argument {
+    public:
+        WrongFramerateException(const int &framerate) : invalid_argument{"Framerate must be higher than 0."}, framerate_{framerate}{};
+        int framerate_;
+        ~WrongFramerateException();
+
+    };
+
+    class NegativeFrameAfterShift : public  MovieSubtitlesError{
+    public:
+        NegativeFrameAfterShift() : MovieSubtitlesError{}{};
+        ~NegativeFrameAfterShift();
+    };
+
+    class SubtitleEndBeforeStart : public MovieSubtitlesError{
+    public:
+        SubtitleEndBeforeStart(const int &line) :  MovieSubtitlesError{}, line_{line}{};
+        ~SubtitleEndBeforeStart();
+        int LineAt() const;
+        int line_;
+    };
+
+    class InvalidSubtitleLineFormat : public MovieSubtitlesError{
+    public:
+        InvalidSubtitleLineFormat() :  MovieSubtitlesError{}{};
+        ~InvalidSubtitleLineFormat();
+    };
 }
 
 
