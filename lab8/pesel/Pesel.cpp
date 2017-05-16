1a6e1d87e5d8351844c7107541fd15a9a10677da
wrobbart <6bartek9@gmail.com>
2017-05-16 18:32:13 +0200

    Merge branch 'kołodziej' of https://github.com/bartekkolodziej/agh-jimp2-grupa4a
    # Conflicts:
    #	lab7/arrayfill/ArrayFill.cpp
    #	lab7/arrayfill/ArrayFill.h
    #	lab7/arrayfill/main.cpp

--- lab8/pesel/Pesel.cpp
+++ lab8/pesel/Pesel.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Pesel.h"
+
+namespace academia {
+    Pesel::Pesel(const char *pesel_) {
+        if (validatePESEL(pesel_) == false) {
+            throw WrongNumbers(pesel_);
+        } else this->pesel_ = pesel_;
+    }
+
+    bool InvalidPeselChecksum();
+    bool InvalidPeselLength();
+    bool InvalidPeselCharacter();
+    bool AcademiaDataValidationError();
+
+    Pesel::~Pesel() {
+    }
+
+    AcademiaDataValidationError::AcademiaDataValidationError(std::string error_message){
+        this->error_message=error_message;
+    }
+
+}
--- lab8/pesel/Pesel.h
+++ lab8/pesel/Pesel.h
@@ -5,8 +5,36 @@
 #ifndef JIMP_EXERCISES_PESEL_H
 #define JIMP_EXERCISES_PESEL_H
 
+#include <stdexcept>
+#include <iostream>
 
+namespace academia {
+    class Pesel {
+    public:
+        Pesel(const char *pesel_);
+        ~Pesel();
+        bool validatePESEL(const char *pesel_);
+        bool InvalidPeselChecksum();
+        bool InvalidPeselLength();
+        bool InvalidPeselCharacter();
+        bool AcademiaDataValidationError();
 
-c
+        const char *pesel_;
+    };
+
+    class AcademiaDataValidationError{
+    public:
+        AcademiaDataValidationError(std::string error_message);
+        virtual  ~AcademiaDataValidationError();
+        std::string error_message;
+    };
+
+    class WrongNumbers : public AcademiaDataValidationError {
+    public:
+        WrongNumbers(const char *pesel_) : AcademiaDataValidationError{"Wrong dimensions of matrix"}, pesel_{pesel_} {}
+
+        Pesel pesel_;
+    };
+}
 
 #endif //JIMP_EXERCISES_PESEL_H
